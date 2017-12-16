48fb987792a2f33c70f05fb928c4a4777fe94ca4
Sandeep Goud <goud8415@gmail.com>
2017-12-16 12:37:50 +0530

    monika project

---
+++ src/firstproject/MyFirstClass.java
@@ -0,0 +1,15 @@
+package firstproject;
+
+public class MyFirstClass {
+/*
+ * This is my first program.
+ * new to java
+ */
+	public static void main(String[] args) {
+		// My First Statement
+		System.out.println("Hello world");
+		
+
+	}
+
+}
